ee28df37986813d8d1598d9b740d7e1c8229e3e2
Ummy-Aimon <ummyatia@gmail.com>
2022-05-05 12:41:09 +0600

    footer added

--- src/Pages/Shared Pages/Footer/Footer.js
+++ src/Pages/Shared Pages/Footer/Footer.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import { FaYoutube } from 'react-icons/fa';
+import { BsFacebook } from 'react-icons/bs';
+import { AiFillInstagram } from 'react-icons/ai';
+import { BsTwitter } from 'react-icons/bs';
+import './Footer.css'
+
 
 const Footer = () => {
+    const today= new Date();
+    const year= today.getFullYear();
     return (
-        <div>
-            <h1>copyright @2022</h1>
-        </div>
+        <div className="footer">         
+         <hr className="text-white"></hr>
+         <div className="">    
+        <p className="text-center fw-bold text-white ">All Rights Reserved © {year} Dr. Mike</p> 
+        
+        <div className="icons-footer">
+                      <div className="icon">
+                      <FaYoutube></FaYoutube>
+                      </div>
+                      <div className="icon">
+                      <BsFacebook></BsFacebook>
+                      </div>
+                      <div className="icon">
+                      <AiFillInstagram></AiFillInstagram>
+                      </div>
+                      <div className="icon">
+                      < BsTwitter></BsTwitter> 
+                      </div>
+                      </div>   
+                  </div>
+        
+        </div> 
     );
 };
 
