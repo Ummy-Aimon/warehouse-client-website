8d4ac34150d115492e14b75a8857029aa2f805fe
Ummy-Aimon <ummyatia@gmail.com>
2022-05-06 00:31:25 +0600

    RequireAuth added

---
+++ src/Component/Loading/Loading.js
@@ -0,0 +1,11 @@
+const Loading = () => {
+    return (
+        <div>
+           <div class="spinner-border text-primary" role="status">
+  <span class="visually-hidden">Loading...</span>
+</div>  
+        </div>
+    );
+};
+
+export default Loading;
