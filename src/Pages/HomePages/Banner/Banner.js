4e821f5a71a2508a52a7700b9eca26e17ded8c35
Ummy-Aimon <ummyatia@gmail.com>
2022-05-05 12:21:36 +0600

    Banner added

--- src/Pages/HomePages/Banner/Banner.js
+++ src/Pages/HomePages/Banner/Banner.js
@@ -10,7 +10,7 @@ const Banner = () => {
           <h1 className="text-banner">Organic Food <span className="text  ">Everyday</span></h1> 
           <p className="text-white fs-4  w-50 fw-bold p-2">Product crafted with care</p> 
           <p className="fs-4 p-2 text-white  fw-bold">Bred for a high content of beneficial substance.Our Products are all fresh and healthy</p>
-          <Button className="button">Shop Now</Button>
+          <Button className="button1">Shop Now</Button>
         </div>
     );
 };
