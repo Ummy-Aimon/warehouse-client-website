6839d1cb6e441e2f47525ec050b2cba84218faf5
Ummy-Aimon <ummyatia@gmail.com>
2022-05-05 10:38:22 +0600

    login added

--- src/Component/Login/Login.js
+++ src/Component/Login/Login.js
@@ -3,11 +3,12 @@ import { Button, Form } from 'react-bootstrap';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import {FcGoogle} from 'react-icons/fc'
 import { useSendPasswordResetEmail, useSignInWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
-import auth from '../../FireBase-init';
+// import auth from '../../FireBase-init';
 import { ToastContainer, toast } from 'react-toastify';
 
   import 'react-toastify/dist/ReactToastify.css';
 import './Login.css'
+import auth from '../../firebase-init';
 
 const Login = () => {
     const[email,setEmail] = useState('')
--- src/Pages/HomePages/About/About.js
+++ src/Pages/HomePages/About/About.js
@@ -18,7 +18,7 @@ const About = () => {
                     <h4>What is our mission?</h4>
                     <p>Our mission is to bring our customers quality products, at the lowest price with the best service in the industry. That is why we are premier online marijuana dispensary! We offer our clients a user-friendly platform where you are only a few clicks away from guaranteed delivery of the highest quality marijuana products right to your front door-step.</p>
                     </div>
-                    <div className="col-md-8">
+                    <div className="col-md-4">
                     <h2 className="mt-5 fw-bold text-warning">Why choose us</h2>
                     <h4>How are we the best?</h4>
                     <p>Customer satisfaction is our top priority, there is nothing more exciting to us than a repeat customer. With our price match policy along with guaranteed Xpresspost delivery, we have been regarded as one of most trusted online marijuana dispensaries. For more information please look at our F.A.Q. page for answers to common questions.</p>
